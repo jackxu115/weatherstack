e385ee37cf499fe8b796a386fae91fbc1572ef0f
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-14 16:00:52 -0700

    update js files

--- js/main.js
+++ js/main.js
@@ -20,34 +20,44 @@ function result (id, result) {
     document.getElementById(id).innerHTML = result;
 }
 
-
-
 function update() {
-    if (element == 'b1'|| 'b2' || 'b3') {
-        if (element == 'b1') {
-            textColor('b1','#ffffff' )
-            textColor('b2','#000000' )
-            textColor('b3','#000000' )
-            bgColor('button1','#f47b21' )
-            bgColor('button2','#ffffff' )
-            bgColor('button3','#ffffff' )
-            result('result', 'Current Weather')
-        } else if (element == 'b2') {
-            textColor('b1','#000000' )
-            textColor('b2','#ffffff' )
-            textColor('b3','#000000' )
-            bgColor('button1','#ffffff' )
-            bgColor('button2','#f47b21' )
-            bgColor('button3','#ffffff' )
-            result('result', 'Weather History')
+
+    console.log(element);
+    let flag = (element === 'b1' || element === 'b2' || element === 'b3');
+    console.log(flag);
+    if (flag) {
+        if (element === 'b1') {
+            textColor('b1','#ffffff' );
+            textColor('b2','#000000' );
+            textColor('b3','#000000' );
+            bgColor('button1','#f47b21' );
+            bgColor('button2','#ffffff' );
+            bgColor('button3','#ffffff' );
+            result('result', 'Current Weather');
+        } else if (element === 'b2') {
+            textColor('b1','#000000' );
+            textColor('b2','#ffffff' );
+            textColor('b3','#000000' );
+            bgColor('button1','#ffffff' );
+            bgColor('button2','#f47b21' );
+            bgColor('button3','#ffffff' );
+            result('result', 'Weather History');
         } else {
-            textColor('b1','#000000' )
-            textColor('b2','#000000' )
-            textColor('b3','#ffffff' )
-            bgColor('button1','#ffffff' )
-            bgColor('button2','#ffffff' )
-            bgColor('button3','#f47b21' )
-            result('result', 'Weather Forecast')
+            textColor('b1','#000000' );
+            textColor('b2','#000000' );
+            textColor('b3','#ffffff' );
+            bgColor('button1','#ffffff' );
+            bgColor('button2','#ffffff' );
+            bgColor('button3','#f47b21' );
+            result('result', 'Weather Forecast');
         }
+    } else {
+        textColor('b1','#000000' );
+        textColor('b2','#000000' );
+        textColor('b3','#000000' );
+        bgColor('button1','#ffffff' );
+        bgColor('button2','#ffffff' );
+        bgColor('button3','#ffffff' );
+        result('result', '');
     }
 }
