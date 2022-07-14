f854780177d99bd7624b5a33289dc491d7a8dd49
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-14 15:32:53 -0700

    add a js file

---
+++ js/main.js
@@ -0,0 +1,53 @@
+console.log("Hello world")
+
+let element;
+
+// get click element information
+window.onclick = function (e) {
+    element = e.target.id;
+    update();
+}
+
+function textColor (id, textColor) {
+    document.getElementById(id).style.color = textColor;
+}
+
+function bgColor (id, bgColor) {
+    document.getElementById(id).style.background = bgColor;
+}
+
+function result (id, result) {
+    document.getElementById(id).innerHTML = result;
+}
+
+
+
+function update() {
+    if (element == 'b1'|| 'b2' || 'b3') {
+        if (element == 'b1') {
+            textColor('b1','#ffffff' )
+            textColor('b2','#000000' )
+            textColor('b3','#000000' )
+            bgColor('button1','#f47b21' )
+            bgColor('button2','#ffffff' )
+            bgColor('button3','#ffffff' )
+            result('result', 'Current Weather')
+        } else if (element == 'b2') {
+            textColor('b1','#000000' )
+            textColor('b2','#ffffff' )
+            textColor('b3','#000000' )
+            bgColor('button1','#ffffff' )
+            bgColor('button2','#f47b21' )
+            bgColor('button3','#ffffff' )
+            result('result', 'Weather History')
+        } else {
+            textColor('b1','#000000' )
+            textColor('b2','#000000' )
+            textColor('b3','#ffffff' )
+            bgColor('button1','#ffffff' )
+            bgColor('button2','#ffffff' )
+            bgColor('button3','#f47b21' )
+            result('result', 'Weather Forecast')
+        }
+    }
+}
