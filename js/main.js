97322439bb014946f85baaffd7e621a485a74d86
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-15 21:43:07 -0700

    add dropdown menu under mobile view

--- js/main.js
+++ js/main.js
@@ -61,3 +61,51 @@ function update() {
         result('result', '');
     }
 }
+
+const burgerMenu = document.getElementById("dropdown");
+const content = document.getElementById("dropdown-content");
+const home = document.querySelector('.home');
+const company = document.querySelector('.company');
+const show = document.querySelector('.show');
+const feature = document.querySelector('.feature');
+const method = document.querySelector('.method');
+const grid = document.querySelector('.grid');
+const client = document.querySelector('.client');
+const access = document.querySelector('.access');
+const footer = document.querySelector('.footer');
+const bgImage = document.querySelector('.background-images');
+
+burgerMenu.addEventListener("click", showContent);
+
+function showContent() {
+    if (content.style.display === 'none') {
+        content.style.display ='flex';
+        home.style.display = 'none';
+        company.style.display = 'none';
+        show.style.display = 'none';
+        feature.style.display = 'none';
+        method.style.display = 'none';
+        grid.style.display = 'none';
+        client.style.display = 'none';
+        access.style.display = 'none';
+        footer.style.display = 'none';
+        bgImage.style.height = '200px';
+    } else {
+        content.style.display = 'none';
+        home.style.display = 'flex';
+        company.style.display = 'flex';
+        show.style.display = 'flex';
+        feature.style.display = 'flex';
+        method.style.display = 'flex';
+        grid.style.display = 'flex';
+        client.style.display = 'flex';
+        access.style.display = 'flex';
+        footer.style.display = 'flex';
+        bgImage.style.height = '900px';
+    }
+
+}
+
+
+
+
